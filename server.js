eb63fa026c42b5d840d8b6fdb6438b1073b22bd3
Michael Amore <michael.amore@dealer.com>
2016-01-26 12:30:33 -0500

    Update API service to allow all origins.

--- server.js
+++ server.js
@@ -9,9 +9,17 @@ mongoose.connect('mongodb://localhost/recipes');
 
 
 // Express
+var allowCrossDomain = function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+}
+
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
 
 
 // Routes
